38f55f75082d9a54689b72621a009e4d61036e53
Juan Andres Villanueva Garzon <jvillanuevag@unal.edu.co>
2025-06-16 14:58:20 -0500

    Web

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return render(request, 'core/index.html')
+
+def saludar(request):
+    return HttpResponse('Hola desde Django!')
+
+def saludar_con_etiqueta(request):
+    return HttpResponse('<h1 style="color:red"> Hola </h1>')
+
+def saludar_con_parametros(request, nombre: str, apellido: str):
+    nombre = nombre.capitalize()
+    apellido = apellido.upper()
+    return HttpResponse(f'{apellido}, {nombre}')
